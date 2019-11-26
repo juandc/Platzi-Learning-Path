04ce1861a6a5501343843bcb9346bf470f54dd7c
Juan David Castro <me@juandc.co>
2019-11-26 11:52:46 -0500

    Two image workflow
    Se genera la imagen como siempre, pero en transparente. Detras de esta imagen puse otra imagen y le tome captura al div contenedor. (sorry por el desorden, es un pre-alpha :P)

--- components/CaptureScreen.js
+++ components/CaptureScreen.js
@@ -26,17 +26,45 @@ const openInNewTab = link =>
 export const CaptureScreen = ({ className, username, items }) => {
   const [imgLink, setImgLink] = useState(null);
   const [showModal, setShowModal] = useState(false);
+
   const captureScreen = () => {
     console.log(html2canvas);
+    // document.body.style.backgroundImage =
+    //   'url("https://images.unsplash.com/photo-1524055988636-436cfa46e59e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80")';
+    // document.body.style.backgroundSize = 'cover';
+    // document.body.style.backgroundRepeat = 'no-repeat';
+    // document.body.style.height = '100vh';
     html2canvas(document.body, {
+      backgroundColor: null,
       scale: 1,
       width: 1000,
-      height: 1000,
+      height: 1250,
       windowWidth: 1000,
-      windowHeight: 1000
+      windowHeight: 1000,
+      imageTimeout: 3000,
+      useCORS: true,
     }).then(c => {
       setImgLink(c.toDataURL());
       setShowModal(true);
+    });
+  };
+
+  const [imgLink2, setImgLink2] = useState(null);
+
+  const captureScreen2 = () => {
+    console.log(html2canvas);
+    html2canvas(document.getElementById('lalala'), {
+      // scale: 10,
+      // width: 250,
+      // scrollY: 5,
+      // height: 350,
+      // windowWidth: 1000,
+      windowHeight: 845,
+      imageTimeout: 0,
+      // imageTimeout: 5000,
+      useCORS: true,
+    }).then(c => {
+      setImgLink2(c.toDataURL());
     });
   };
 
@@ -51,11 +79,32 @@ export const CaptureScreen = ({ className, username, items }) => {
         <MdImage className="m-auto" />
       </ActionButton>
       <Modal onOutClick={handleModal} show={showModal}>
-        <div className="w-full max-w-md h-auto p-6 pb-0">
+        <div className="w-full max-w-md h-auto p-6 pb-0" style={{ paddingTop: '0'}}>
+          <div id="lalala" style={{ width: '400px', height: '500px', backgroundSize: 'cover', backgroundImage: 'url("https://instagram.fbog11-1.fna.fbcdn.net/v/t51.2885-15/e35/74339842_940213813019741_3965249199834684398_n.jpg?_nc_ht=instagram.fbog11-1.fna.fbcdn.net&_nc_cat=105&oh=869d54f54284fa6685a493b90ac1a20f&oe=5E6DADF7")' }}>
+          {/* <img
+            style={{
+              position: 'absolute',
+              zIndex: -1,
+              width: '400px',
+              height: '400px',
+            }}
+            src="https://images.unsplash.com/photo-1524055988636-436cfa46e59e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+            className="shadow-lg border-gray-300 border-solid border-2"
+          /> */}
           <img
             src={imgLink}
             alt=""
-            className="shadow-lg border-gray-300 border-solid border-2"
+            className="border-gray-300 border-solid border-2"
+            />
+          </div>
+
+        </div>
+        <div className="w-full max-w-md h-auto p-6 pb-0" >
+          <img
+            src={imgLink2}
+            alt=""
+            className="border-gray-300 border-solid border-2"
+            style={{ width: '400px' }}
           />
         </div>
         <div className="flex align-middle text-center px-6 my-2">
@@ -66,6 +115,10 @@ export const CaptureScreen = ({ className, username, items }) => {
             <MdSave className="m-auto" />
           </ActionButton>
           <span className="my-auto md:text-sm">Guardar Imagen</span>
+
+          <br />
+
+          <button onClick={captureScreen2}>Segunda imagen</button>
         </div>
       </Modal>
     </div>
